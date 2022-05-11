7f8b22573ad0e1e4d6924685e1d69b908943060a
KmilaC <98900343+Milakmila@users.noreply.github.com>
2022-05-11 08:33:46 -0500

    Upload login .js

--- js/login.js
+++ js/login.js
@@ -1,11 +1,19 @@
-let user;
-let password;
+let button=document.getElementById("ingresar");
+let res=document.getElementById('res');
 
-document.querySelector()
+button.onclick=function login(e){
+    e.preventDefault()
+    let user=document.getElementById("nombre").value;
+    let password=document.getElementById("password").value; 
 
-user=prompt("Digite Usuario");
-password=prompt("Digite Usuario");
-
-if(user==="Admin" &&  password==="Kuepa2022"){
-    console.log("Es correcto");
+    if(user==="Admin" &&  password==="Kuepa2022"){
+    window.location="dashboard.html"
+    }else{
+        res.textContent="Por favor ingrese correactemente password y Usuario"
+        res.style.setProperty("visibility", "visible");
+        res.style.textAlign="center";
+        res.style.color="#A40101";
+        res.style.fontWeight="800";
+        res.style.setProperty("opacity", "1");
+    }
 }
